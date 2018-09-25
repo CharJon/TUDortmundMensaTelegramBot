672fe68812117d317d471233c343801b6e3071cd
Jonas Charfreitag <j.charfreitag@gmail.com>
2018-09-25 10:26:51 +0200

    Updates main.

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ import mensa_bot as mb
 
 if __name__ == '__main__':
     tu_mensa_bot = mb.MensaBot()
-    try:
-        tu_mensa_bot.start_bot()
-    except KeyboardInterrupt:
-        print("Exit Because of KeyboardInterrupt.")
-        exit()
+    tu_mensa_bot.start_bot()
